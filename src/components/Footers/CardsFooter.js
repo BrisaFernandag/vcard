7f7f2a8c8a97ae6ad84e7a6a23eab5ce45a682ad
Jesus Ramirez Ayala <jramirezayala1298@gmail.com>
2023-01-09 10:47:15 -0600

    first commit

---
+++ src/components/Footers/CardsFooter.js
@@ -0,0 +1,121 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+import { Link } from "react-router-dom";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardImg,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip
+} from "reactstrap";
+
+class CardsFooter extends React.Component {
+  render() {
+    return (
+      <>
+        <footer className="footer">
+          <Container>
+            <hr />
+            <Row className="align-items-center justify-content-md-between">
+              <Col md="6">
+                <div className="copyright">
+                  © {new Date().getFullYear()}{" "}
+                  <a
+                    href="https://www.creative-tim.com?ref=adsr-footer"
+                    target="_blank"
+                  >
+                    Business in Motion
+                  </a>
+                  .
+                </div>
+              </Col>
+              <Col className="text-lg-right btn-wrapper" md="6">
+                <Button
+                  className="btn-icon-only rounded-circle"
+                  color="twitter"
+                  href="https://twitter.com/creativetim"
+                  id="tooltip475038074"
+                  target="_blank"
+                >
+                  <span className="btn-inner--icon">
+                    <i className="fa fa-twitter" />
+                  </span>
+                </Button>
+                <UncontrolledTooltip delay={0} target="tooltip475038074">
+                  Follow us
+                </UncontrolledTooltip>
+                <Button
+                  className="btn-icon-only rounded-circle ml-1"
+                  color="facebook"
+                  href="https://www.facebook.com/creativetim"
+                  id="tooltip837440414"
+                  target="_blank"
+                >
+                  <span className="btn-inner--icon">
+                    <i className="fa fa-facebook-square" />
+                  </span>
+                </Button>
+                <UncontrolledTooltip delay={0} target="tooltip837440414">
+                  Like us
+                </UncontrolledTooltip>
+                <Button
+                  className="btn-icon-only rounded-circle ml-1"
+                  color="dribbble"
+                  href="https://dribbble.com/creativetim"
+                  id="tooltip829810202"
+                  target="_blank"
+                >
+                  <span className="btn-inner--icon">
+                    <i className="fa fa-dribbble" />
+                  </span>
+                </Button>
+                <UncontrolledTooltip delay={0} target="tooltip829810202">
+                  Follow us
+                </UncontrolledTooltip>
+                <Button
+                  className="btn-icon-only rounded-circle ml-1"
+                  color="github"
+                  href="https://github.com/creativetimofficial"
+                  id="tooltip495507257"
+                  target="_blank"
+                >
+                  <span className="btn-inner--icon">
+                    <i className="fa fa-github" />
+                  </span>
+                </Button>
+                <UncontrolledTooltip delay={0} target="tooltip495507257">
+                  Star on Github
+                </UncontrolledTooltip>
+              </Col>
+            </Row>
+          </Container>
+        </footer>
+      </>
+    );
+  }
+}
+
+export default CardsFooter;
